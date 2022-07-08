b2a526365f1255ad21393dacafed41c1fc3d7e40
Skatler <yaroslavzozuliya95@gmail.com>
2022-07-08 13:54:10 +0300

    My training project

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 "user strict";
 
-let i = 0;
-do {
-  alert(i);
-  i++;
-} while (i < 3);
+const numberOfFilm = prompt("How many movies have you seen so far ?", "");
+const personalMovieDB = {
+  count: numberOfFilm,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const lastMoviesOne = prompt("One of the last you saw ?", "");
+const rateMoviesOne = prompt("How much do you rate this movies ?", "");
+const lastMoviesTwo = prompt("One of the last you saw ?", "");
+const rateMoviesTwo = prompt("How much do you rate this movies ?", "");
+
+personalMovieDB.movies[lastMoviesOne] = rateMoviesOne;
+personalMovieDB.movies[lastMoviesTwo] = rateMoviesTwo;
+console.log(personalMovieDB);
