7451d3c8e6bc964ea1b9254dcca91316a634479c
Skatler <yaroslavzozuliya95@gmail.com>
2022-07-14 17:17:11 +0300

    New object

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"user strict";
+"use strict";
 
 const numberOfFilm = prompt("How many movies have you seen so far ?", "");
 const personalMovieDB = {
@@ -9,11 +9,25 @@ const personalMovieDB = {
   privat: false,
 };
 
-const lastMoviesOne = prompt("One of the last you saw ?", "");
-const rateMoviesOne = prompt("How much do you rate this movies ?", "");
-const lastMoviesTwo = prompt("One of the last you saw ?", "");
-const rateMoviesTwo = prompt("How much do you rate this movies ?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("One of the last you saw ?", "");
+  const b = prompt("How much do you rate this movies ?", "");
 
-personalMovieDB.movies[lastMoviesOne] = rateMoviesOne;
-personalMovieDB.movies[lastMoviesTwo] = rateMoviesTwo;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+if (personalMovieDB.count < 10) {
+  console.log("Few movies.");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("You are a classic viewer.");
+} else if (personalMovieDB.count >= 30) {
+  console.log("You're a movie buff");
+} else {
+  console.log("Error");
+}
 console.log(personalMovieDB);
