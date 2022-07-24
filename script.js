642b24507051f70b889963c883a60d3ec7c76fcb
Skatler <yaroslavzozuliya95@gmail.com>
2022-07-24 12:10:53 +0300

    Обновление

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 "use strict";
 
-const numberOfFilm = prompt("How many movies have you seen so far ?", "");
+const numberOfFilms = +prompt("How many movies have you seen?", "");
 const personalMovieDB = {
-  count: numberOfFilm,
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("One of the last you saw ?", "");
-  const b = prompt("How much do you rate this movies ?", "");
+const questionOne = prompt("One of the last movies you watched?", "");
+const questionTwo = prompt("How much do you rate this movie?", "");
+const questionThree = prompt("One of the last movies you watched?", "");
+const questionFour = prompt("How much do you rate this movie?", "");
 
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("Error");
-    i--;
-  }
-}
-if (personalMovieDB.count < 10) {
-  console.log("Few movies.");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("You are a classic viewer.");
-} else if (personalMovieDB.count >= 30) {
-  console.log("You're a movie buff");
-} else {
-  console.log("Error");
-}
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionThree] = questionFour;
+
 console.log(personalMovieDB);
